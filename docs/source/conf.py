e57961467d4957565ff75f1ee180c53240575c4b
Olivier <leveque.olivier@icloud.com>
2025-08-06 11:54:02 +0200

    Update copyright and version information in Sphinx configuration

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,9 +7,10 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'SAR Geometry'
-copyright = 'ONERA/DEMR'
+copyright = '2024, Olivier LEVEQUE'
 author = 'Olivier LEVEQUE'
-release = '0.0.1'
+release = '0.2.0'
+version = '0.2.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
